4d4617dde0549bd7653dc85dd6fca5aaa69327d8
Afiq Hamzah <afiq.hamzah@nazrol.tech>
2019-12-27 19:05:57 +0800

    chore: add resume page, now default redirect to resume page

---
+++ src/pages/Resume.vue
@@ -0,0 +1,30 @@
+
+<template>
+  <div class="container is-fluid">
+    <main-info />
+    <education />
+    <employment />
+    <skill />
+  </div>
+</template>
+
+<script>
+
+import mainInfo from "~/components/resume/MainInfo.vue";
+import education from "~/components/resume/Education.vue";
+import employment from "~/components/resume/Employment.vue";
+import skill from "~/components/resume/Skill.vue";
+
+export default {
+    components : {
+        mainInfo,
+        education,
+        employment,
+        skill,
+    }
+};
+
+</script>
+
+<style scoped>
+</style>
